4515161ab8b549ae92ae53096ab1b28ab4368348
Saulo Marcos Corrêa <sauloejrv@gmail.com>
2019-03-19 22:26:38 -0300

    Utilizando o decorator @Input()

--- academy/src/app/student/student.component.ts
+++ academy/src/app/student/student.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'aca-student',
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  name: string = 'Saulo'
-  isMatriculado: boolean = true
+  @Input() name: string
+  @Input() isMatriculado: boolean
 
   constructor() { }
 
