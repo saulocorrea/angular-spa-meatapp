0e41fc060a7ce4e7d1799b70b840c87549012cc2
Saulo Marcos Corrêa <sauloejrv@gmail.com>
2019-03-19 21:57:57 -0300

    Adicionando arquivos criados pelo angular-cli.

---
+++ academy/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aca-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'academy';
+}
