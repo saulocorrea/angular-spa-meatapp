73449c0a27c73611432c139c466b00072a5b8d44
Saulo Marcos Corrêa <saulo@tinotech.com.br>
2019-04-06 21:05:26 -0300

    Trabalhando com ROTAS CHILDREN em um componente [restaurants/:id/menu, restaurants/:id/reviews].

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { RestaurantDetailComponent } from './restaurants/restaurant-detail/resta
 import { MenuComponent } from './restaurants/restaurant-detail/menu/menu.component';
 import { CarrinhoComponent } from './restaurants/restaurant-detail/carrinho/carrinho.component';
 import { MenuItemComponent } from './restaurants/restaurant-detail/menu-item/menu-item.component';
+import { ReviewsComponent } from './restaurants/restaurant-detail/reviews/reviews.component';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import { MenuItemComponent } from './restaurants/restaurant-detail/menu-item/men
     RestaurantDetailComponent,
     MenuComponent,
     CarrinhoComponent,
-    MenuItemComponent
+    MenuItemComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
