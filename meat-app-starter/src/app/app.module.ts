037354b8d34289f8df98ac58219b95e8b4f93a48
Saulo Marcos Corrêa <saulo@tinotech.com.br>
2021-05-05 16:18:37 -0300

    Adicionado pré-carregamento de módulos lazzy load

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-
+import { PreloadAllModules, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -37,11 +36,11 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     HttpModule,
     FormsModule,
-    RouterModule.forRoot(ROUTES),
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules }),
     SharedModule.forRoot() //importa todos os Components AND os Servicos do modulo Shared (ModuleWithProviders)
   ],
   providers: [
-    {provide: LOCALE_ID, useValue: 'pt-BR'}
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ],
   bootstrap: [AppComponent]
 })
